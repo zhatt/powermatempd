4a0f8ef9628daefb2396eb9d4c9446a58250eb6c
Craig Robson <craig@zhatt.com>
2011-03-31 07:31:33 -0700

    Working on fakepm select loop.

--- sample/fakepm.c
+++ sample/fakepm.c
@@ -1,5 +1,12 @@
 
+#include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <errno.h>
+
 #include <linux/input.h>
 #include <assert.h>
 
@@ -14,10 +21,39 @@ static const struct input_event events[] = {
 
 int main() {
 	unsigned num_events = sizeof(events) / sizeof(events[0]);
+	int rfd = open( "/tmp/xxxread", O_RDONLY | O_NONBLOCK );  // FIXME
+	int wfd = open( "/tmp/xxxwrite", O_WRONLY );
+	assert( rfd != -1 );
+	assert( wfd != -1 );
 
 	for ( unsigned i = 0; i < num_events; i++ ) {
-		int ret = write( 1, &events[i], sizeof(events[i]));
+		struct timeval tv = { .tv_sec = 10, .tv_usec = 10000 };
+
+		while ( tv.tv_sec || tv.tv_usec ) {
+			fd_set rfds;
+			fd_set wfds;
+
+			FD_ZERO(&rfds);
+			FD_ZERO(&wfds);
+			FD_SET(rfd, &rfds);
+			//			FD_SET(wfd, &wfds);
+
+			printf("Loop1\n");
+			int ret = select((rfd > wfd ? rfd : wfd)+1, &rfds, &wfds, NULL, &tv);
+			printf("Select %d\n", ret);
+			assert( ret != -1 );
+
+			if ( FD_ISSET( rfd, &rfds ) ) {
+				char* buf[2];
+				int bytes = read ( rfd, buf, 2 );
+				printf("Read %d\n", bytes);
+			}
+
+			printf("Loop2\n");
+		}
+		int ret = write( wfd, &events[i], sizeof(events[i]));
 		assert( ret == sizeof(events[i]) );
+		printf("write\n");
 	}
 
 	return 0;
